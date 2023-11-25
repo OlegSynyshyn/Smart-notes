a80303b2c388ddfe6474833a8bd3c4ccac18dae7
OlegSynyshyn <145208359+OlegSynyshyn@users.noreply.github.com>
2023-11-25 17:38:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from PyQt5.QtWidgets import QApplication, QTextEdit, QListWidget, QTextEdit, QWidget, QPushButton, QLineEdit, QListWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import (QApplication, 
+QTextEdit, QListWidget, QTextEdit, QWidget, 
+QPushButton, QLineEdit, QListWidget, QHBoxLayout, 
+QVBoxLayout, QInputDialog)
+import json
 app = QApplication([])
 window = QWidget()
 
@@ -39,22 +43,47 @@ line2.addWidget(add_teg_btn)
 line2.addWidget(unpin_teg_btn)
 line2.addWidget(search_teg_btn)
 
+notes = {}
+
+
+def writeFile():
+    with open("notes.json", "w", encoding="utf-8") as file:
+        json.dump(notes, file, ensure_ascii=True, sort_keys=True, indent=4)
 
 
 
+main_line.addLayout(line1, stretch=2)
+main_line.addLayout(line2, stretch=1)
+window.setLayout(main_line)
+def show_note():
+    note_name = notes_list.currentItem().text()
+    print(notes)
+    text.setText(notes[note_name]['text'])
 
 
+notes_list.itemClicked.connect(show_note)
+notes_list.addItems(notes)
 
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Нова замітка", "Введіть назву замітки")
+    if ok and note_name != "":
+        notes[note_name] = {
+            "text": "", 
+            "tags": [],
+        }
+        notes_list.addItem(note_name)
+create_note_btn.clicked.connect(add_note)
 
+with open("notes.json", "r", encoding="utf-8") as file:
+    notes = json.load(file)
 
-
-
-
-
-
-
-
-
+notes_list.addItems(notes)
+def save_note():
+    note_text = text.toPlainText()
+    note_name = notes_list.currentItem().text()
+    notes[note_name]['text'] = note_text
+    writeFile()
+save_note_btn.clicked.connect(save_note)
 
 
 window.show()
